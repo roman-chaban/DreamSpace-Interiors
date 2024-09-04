a869b1e41a80b16437bff94a5866ec6fe3f409a8
Roman Chaban <roman.chaban1001@gmail.com>
2024-09-04 14:39:12 -0800

    Added ProductGallery component

--- src/components/product/ProductGallery/ProductGallery.tsx
+++ src/components/product/ProductGallery/ProductGallery.tsx
@@ -1,11 +1,32 @@
 import type { FC } from 'react';
 import styles from './ProductGallery.module.scss';
 import { Product } from '@/types/products';
+import Image from 'next/image';
 
 export type ProductGalleryType = {
   product: Product;
 };
 
 export const ProductGallery: FC<ProductGalleryType> = ({ product }) => {
-  return <div className={styles.gallery}>Product Gallery</div>;
+  return (
+    <div className={styles.gallery}>
+      <div className={styles.galleryMainImage}>
+        <Image
+          src={product.imageUrl}
+          alt={product.title}
+          width={400}
+          height={400}
+          className={styles.mainImage}
+        />
+        <div className={styles.discountTitles}>
+          <span className={styles.discountedTitle}>
+            {product.discountedTitle}
+          </span>
+          <span className={styles.discountPercentage}>
+            {product.discountPercentage}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
