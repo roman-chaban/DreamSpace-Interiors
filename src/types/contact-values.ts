a86fd866779ad258160acddee531b0faeb68d0e0
Roman Chaban <roman.chaban1001@gmail.com>
2024-09-06 21:44:23 -0800

    Corrected classNames path for valuesClassNames

--- src/types/contact-values.ts
+++ src/types/contact-values.ts
@@ -1,5 +1,5 @@
 import styles from '@/components/contact/ContactUs/ContactUs.module.scss';
-import { ClassNames } from '@/components/layout/ValueItem/ValueItem';
+import { ClassNames } from './value-item';
 
 export const valuesClassNames: ClassNames = {
   classNames: {
